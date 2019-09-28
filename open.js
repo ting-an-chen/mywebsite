16e829e762e035c4aa26b98fad1c80f4bcd0726c
ting-an-chen <ting-an-chen@users.noreply.github.com>
2019-09-28 23:38:34 +0800

    Update open.js

--- open.js
+++ open.js
@@ -15,13 +15,9 @@ function openPage(divID) {
 }
 
 function w3_open() {
-  document.getElementById("main").style.marginLeft = "25%";
-  document.getElementById("mySidebar").style.width = "25%";
   document.getElementById("mySidebar").style.display = "block";
-  document.getElementById("openNav").style.display = "none";
 }
+
 function w3_close() {
-  document.getElementById("main").style.marginLeft = "0%";
   document.getElementById("mySidebar").style.display = "none";
-  document.getElementById("openNav").style.display = "inline-block";
 }
